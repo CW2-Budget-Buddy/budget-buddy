592c31a06e6a758972e759cfdc35119d0578ca35
krishalika <krishalikadilanirathnayake@gmail.com>
2025-04-06 22:49:12 +0800

    Initial Commit

---
+++ budget-buddy/budget_buddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  budget_buddyApp.swift
+//  budget-buddy
+//
+//  Created by user261989 on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct budget_buddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
